d1005b17526c94b805e6731ad7af4f842b0e1098
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-01-26 10:35:18 +0100

    Use flit instead of poetry

--- src/geoarrow/__init__.py
+++ src/geoarrow/__init__.py
@@ -1,3 +1,5 @@
-# read version from installed package
-from importlib.metadata import version
-__version__ = version("geoarrow")
+"""
+Storing geometry data in Apache Arrow format
+"""
+
+__version__ = "0.1.0"
